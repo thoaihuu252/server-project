b63835900037ee52f5be6345fc48207cdb5fe91d
PhamHuuThoai <thoaihuu252@gmail.com>
2024-04-05 22:16:42 +0700

    Build Spring

---
+++ src/main/java/com/itboy/DACNPM/DacnpmApplication.java
@@ -0,0 +1,13 @@
+package com.itboy.DACNPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacnpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacnpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itboy/DACNPM/DacnpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itboy.DACNPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacnpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
